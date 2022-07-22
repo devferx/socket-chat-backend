6182916c1f399ac235f5f81f3561599b271d05f3
devferx <ferkki@hotmail.com>
2022-07-22 16:09:36 -0400

    Crear usuario en base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,34 @@
 const { request, response } = require("express");
+const User = require("../models/user");
 
 async function createUser(req, res = response) {
-  res.json({
-    ok: true,
-    msg: "User created",
-    user: undefined,
-  });
+  try {
+    const { email, name, password } = req.body;
+
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The email already exists",
+      });
+    }
+
+    const user = new User({ email, name, password });
+    await user.save();
+
+    res.json({
+      ok: true,
+      msg: "User created",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Error in the server",
+    });
+  }
 }
 
 async function login(req = request, res = response) {
