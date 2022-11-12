95c536cb0355c75872ade827ab8bdf3790589e67
devferx <ferkki@hotmail.com>
2022-11-11 22:48:23 -0400

    Identificar persona conectada - Backend

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,5 @@
+const { verifyJWT } = require("../helpers/jwt");
+
 class Sockets {
   constructor(io) {
     this.io = io;
@@ -6,6 +8,14 @@ class Sockets {
 
   socketEvents() {
     this.io.on("connection", (socket) => {
+      const [isValid, uid] = verifyJWT(socket.handshake.query["x-token"]);
+
+      if (!isValid) {
+        console.log("socket no identificado");
+        return socket.disconnect();
+      }
+
+      console.log("Cliente conectado", uid);
       // TODO: Validate JWT
       // If token is not valid, disconnect socket
       // TODO: View who user is connected
@@ -15,6 +25,10 @@ class Sockets {
       // TODO: Disconnet
       // Set in the DB that the user disconneted
       // TODO: Emit all user conected
+
+      socket.on("disconnect", () => {
+        console.log("Cliente desconectado", uid);
+      });
     });
   }
 }
